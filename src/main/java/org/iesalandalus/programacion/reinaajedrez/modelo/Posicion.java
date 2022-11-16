d3deefde932000145acdc4fcd6c4fe1b17927e5e
Miguel <miguelsf12@hotmail.es>
2022-11-16 20:24:55 +0100

    Creacion set y get de los atributos

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -5,5 +5,39 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	public void setFila(int fila) {
+		
+		if(fila < 1 || fila > 8) {
+			
+			throw new IllegalArgumentException("La filas deben tener un valor entre 1 y 8");
+			
+		}
+		else {
+			
+			this.fila = fila;
+		}
+	}
+	
+	public int getFila() {
+		
+		return fila;
+	}
+	
+	public void setColumna(char columna) {
+		
+		if(columna < 'a'|| columna > 'h') {
+			
+			throw new IllegalArgumentException("Las columnas deben ser desde la 'a' a la 'h'");
+			
+		}
+		else {
+			
+			this.columna = columna;
+			
+		}
+	}
+	
+	
+	
 
 }
