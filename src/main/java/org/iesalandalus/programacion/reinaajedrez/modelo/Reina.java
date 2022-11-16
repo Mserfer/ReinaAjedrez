573bdf1a785024b3a0e70514808ebf9e63aae1d7
Miguel <miguelsf12@hotmail.es>
2022-11-16 20:44:43 +0100

    Creados atributos color y posicion

---
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -0,0 +1,16 @@
+package org.iesalandalus.programacion.reinaajedrez.modelo;
+
+public class Reina {
+	
+	private static final Color COLOR = Color.BLANCO;
+	
+	private Posicion posicion;
+	private Color color;
+	
+	
+	
+	
+	
+	
+
+}
